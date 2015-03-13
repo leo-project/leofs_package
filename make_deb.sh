0e817e1a75eca6f915833e6c1cf995448cd5eb1a
firejun <fire@jun-t.com>
2015-03-13 14:43:37 +0900

    Initial commit

---
+++ make_deb.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+export LANG=C
+DT1=`date | awk '{print $1", "$3" "$2" "$6" "$4" +0900"}'`
+DT2=`date | awk '{print $6}'`
+LEOFS=leofs-${1}
+
+git clone https://github.com/leo-project/leofs.git
+
+mv leofs ${LEOFS}
+
+cd ${LEOFS}
+git checkout ${1}
+
+mkdir -p debian
+#cd debian
+
+cat << EOT >> debian/changelog
+leofs (${1}-1) unstable; urgency=low
+
+  * Initial release.
+
+ -- Jun TAKIZAWA <leofs@leofs.org>  ${DT1}
+EOT
+
+cat << 'EOT' >> debian/control
+Source: leofs
+Section: unknown
+Priority: optional
+Maintainer: Jun TAKIZAWA <leofs@leofs.org>
+Build-Depends: debhelper (>= 8.0.0)
+Standards-Version: 3.9.2
+Homepage: http://www.leofs.org/
+#Vcs-Git: git://git.debian.org/collab-maint/leofs.git
+#Vcs-Browser: http://git.debian.org/?p=collab-maint/leofs.git;a=summary
+
+Package: leofs
+Architecture: any
+Depends: ${shlibs:Depends}
+#, ${misc:Depends}
+Description: LeoFS is the Web shaped object storage system.
+ LeoFS is the Web shaped object storage system and S3 compatible storage.
+
+#Package: leofs-doc
+#Architecture: all
+#Description: documentation for leofs
+# documentation for leofs
+EOT
+
+cat << EOT >> debian/copyright
+Format: http://dep.debian.net/deps/dep5
+Upstream-Name: leofs
+Source: http://www.leofs.org/
+
+Files: *
+Copyright: ${DT2} Jun TAKIZAWA <leofs@leofs.org>
+License: Apache-2.0
+
+Files: debian/*
+Copyright: ${DT2} Jun TAKIZAWA <leofs@leofs.org>
+License: Apache-2.0
+
+License: Apache-2.0
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ .
+ http://www.apache.org/licenses/LICENSE-2.0
+ .
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ .
+ On Debian systems, the complete text of the Apache version 2.0 license
+ can be found in "/usr/share/common-licenses/Apache-2.0".
+
+# Please also look if there are files or directories which have a
+# different copyright/license attached and list them here.
+EOT
+
+cat << 'EOT' >> debian/rules
+#!/usr/bin/make -f
+# -*- makefile -*-
+# Sample debian/rules that uses debhelper.
+#
+# This file was originally written by Joey Hess and Craig Small.
+# As a special exception, when this file is copied by dh-make into a
+# dh-make output file, you may use that output file without restriction.
+# This special exception was added by Craig Small in version 0.37 of dh-make.
+#
+# Modified to make a template file for a multi-binary package with separated
+# build-arch and build-indep targets  by Bill Allombert 2001
+
+# Uncomment this to turn on verbose mode.
+#export DH_VERBOSE=1
+
+# This has to be exported to make some magic below work.
+export DH_OPTIONS
+
+
+#%:
+#	dh $@
+
+package = leofs
+EOT
+cat << EOT >> debian/rules
+version = ${1}
+EOT
+cat << 'EOT' >> debian/rules
+docdir = debian/tmp/usr/share/doc/$(package)
+installdir = debian/tmp/usr/local/$(package)/$(version)
+bindir = debian/tmp/usr/local/bin
+
+build:	binary
+
+clean:
+	$(MAKE) clean
+	rm -rf packages *~ debian/tmp debian/*~ debian/files* debian/substvars
+
+binary-indep:	checkroot build
+
+binary-arch:	checkroot build
+	rm -rf debian/tmp
+	mkdir -p $(installdir)
+	mkdir -p $(bindir)
+	$(MAKE)
+	$(MAKE) release
+	cp -r package/* $(installdir)
+	cp leofs-adm $(bindir)
+	dpkg-shlibdeps $(installdir)/leo_manager_0/erts-6.3/bin/beam
+	mkdir -p debian/tmp/DEBIAN
+	dpkg-gencontrol
+	chown -R root:root debian/tmp
+	chmod -R u+w,go=rX debian/tmp
+	dpkg-deb --build debian/tmp ..
+
+binary:	binary-indep binary-arch
+
+checkroot:
+	test $$(id -u) = 0
+
+.PHONY: binary binary-arch binary-indep clean checkroot
+EOT
+
+chmod 755 debian/rules
+
+fakeroot debian/rules
+
+cd ..
+rm -rf ${LEOFS}
